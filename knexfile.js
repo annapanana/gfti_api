f4cc64091dcaaa0682be082d1af01444421bfd70
Anna Lotko <annaklotko@gmail.com>
2018-06-29 13:32:12 -0600

    added production environment to knexfile

--- knexfile.js
+++ knexfile.js
@@ -4,5 +4,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/gfti'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 };
