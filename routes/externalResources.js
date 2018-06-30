8a3acb1f152fba72e2fba5ad8b6f3293499c2ca8
Anna Lotko <annaklotko@gmail.com>
2018-06-30 14:55:41 -0600

    address validation working

--- routes/externalResources.js
+++ routes/externalResources.js
@@ -5,9 +5,9 @@ require('dotenv').load();
 const request = require('request');
 
 router.post('/', (req, res, next) => {
-  const {zip, resource} = req.body;
-  // TODO - use resource to delegate to methods
-  const url = `https://www.googleapis.com/civicinfo/v2/representatives?key=${process.env.GOOGLE_API_KEY}&address=${zip}`
+  const {resource} = req.body,
+        url = getResourceMethod(resource)(req.body);
+
   request(url, (error, response, body) => {
     if (!error && response.statusCode == 200) {
       res.status(200).send(body)
@@ -16,6 +16,31 @@ router.post('/', (req, res, next) => {
     }
   })
 })
+
+function getResourceMethod(resource) {
+  const resourceMethods = [
+    {key: "rep-by-zip", method:getReps},
+    {key: "validate-address", method:validateAddress}
+  ]
+  return resourceMethods.find(elem => {
+    return elem.key === resource
+  }).method
+}
+
+function getReps(body) {
+  const zip = body['params[zip]'];
+  return `https://www.googleapis.com/civicinfo/v2/representatives?key=${process.env.GOOGLE_API_KEY}&address=${zip}`
+}
+
+function validateAddress(body) {
+  const address1 = body['params[address][address1]'],
+        address2 = body['params[address][address2]'],
+        state = body['params[address][state]'],
+        city = body['params[address][city]'],
+        zip = body['params[address][zip]'];
+
+  return `https://us-street.api.smartystreets.com/street-address?auth-id=${process.env.SMARTY_ID}&auth-token=${process.env.SMARTY_TOKEN}&candidates=10&street=${address1}&street2=${address2}&city=${city}&state=${state}&zipcode=${zip}`;
+}
 
 
 module.exports = router;
