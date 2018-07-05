6f12460b3f7dabe9174e815f41c0c8aa6b5b21c2
Anna Lotko <annaklotko@gmail.com>
2018-07-04 18:14:43 -0600

    postcard image saving to lob template and pushing to external log api

--- routes/cards.js
+++ routes/cards.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const knex = require('../knex');
 require('dotenv').load();
 const Lob = require('lob')(process.env.LOB_KEY);
+const fs = require('fs');
 
 router.get('/', (req, res, next) => {
   knex('cards')
@@ -18,16 +19,25 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/', (req, res, next) => {
+  console.log("POST CARD");
   const {address, bg_img, pc_back} = req.body;
+  // image_url
+  // greetings_text
+  let postcard_front = fs.readFileSync(__dirname + `/../public/templates/basic_postcard.html`, { encoding: 'utf-8' });
+  console.log(postcard_front);
   const card = Lob.postcards.create({
     description: 'Demo Postcard job',
     to: address,
-    front: '<html style="padding: 1in; font-size: 50;">Front HTML for {{name}}</html>',
+    front: postcard_front,
     back: '<html style="padding: 1in; font-size: 20;">Back HTML for {{name}}</html>',
     merge_variables: {
-      name: address.name
+      name: address.name,
+      image_url: bg_img,
+      greetings_text: "Hello World!"
     }
   }, function (error, result) {
+    console.log("ERROR", error);
+    console.log("RESULT", result);
     if (error) {
       return res.send(error)
     } else {
