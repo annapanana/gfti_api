ecd010437612638aa4f97f7202f87bfea9bc51d3
Anna Lotko <annaklotko@gmail.com>
2018-07-04 18:55:04 -0600

    removed old address api

--- routes/externalResources.js
+++ routes/externalResources.js
@@ -5,8 +5,8 @@ require('dotenv').load();
 const request = require('request');
 
 router.post('/', (req, res, next) => {
-  const {resource} = req.body,
-        url = getResourceMethod(resource)(req.body);
+  const {resource} = req.body;
+  const url = getResourceMethod(resource)(req.body);
   request(url, (error, response, body) => {
     if (!error && response.statusCode == 200) {
       res.status(200).send(body)
@@ -19,7 +19,6 @@ router.post('/', (req, res, next) => {
 function getResourceMethod(resource) {
   const resourceMethods = [
     {key: "rep-by-zip", method:getReps},
-    {key: "validate-address", method:validateAddress},
     {key: "unsplash-photos", method:getUnsplashPhotos}
   ]
   return resourceMethods.find(elem => {
@@ -30,17 +29,6 @@ function getResourceMethod(resource) {
 function getReps(body) {
   const {zip} = body.params;
   return `https://www.googleapis.com/civicinfo/v2/representatives?key=${process.env.GOOGLE_API_KEY}&address=${zip}`
-}
-
-function validateAddress(body) {
-  const {address1, address2, state, city, zip} = body.params.address;
-  // const address1 = body['params[address][address1]'],
-  //       address2 = body['params[address][address2]'],
-  //       state = body['params[address][state]'],
-  //       city = body['params[address][city]'],
-  //       zip = body['params[address][zip]'];
-
-  return `https://us-street.api.smartystreets.com/street-address?auth-id=${process.env.SMARTY_ID}&auth-token=${process.env.SMARTY_TOKEN}&candidates=10&street=${address1}&street2=${address2}&city=${city}&state=${state}&zipcode=${zip}`;
 }
 
 function getUnsplashPhotos(body) {
